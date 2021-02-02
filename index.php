d3dbe1ecd17311f73730aa8a1a4ab3bb88ab4684
y2k6678 <sand_6678@hotmail.com>
2021-02-02 10:46:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
-<?php 
-define(‘LINE_API’,”https://notify-api.line.me/api/notify");
+<?php
+define('LINE_API',"https://notify-api.line.me/api/notify");
  
-$token = “Bearer PIGX1czxHELPwV8XpSdtgBsSmU292IljgJIT4IhUQLn”; //ใส่Token ที่copy เอาไว้
-$str = “Hello”; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+$token = "xxxxxxx"; //ใส่Token ที่copy เอาไว้
+$str = "devbanban Test Line Notify API "; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
  
 $res = notify_message($str,$token);
 print_r($res);
 function notify_message($message,$token){
- $queryData = array(‘message’ => $message);
- $queryData = http_build_query($queryData,’’,’&’);
+ $queryData = array('message' => $message);
+ $queryData = http_build_query($queryData,'','&');
  $headerOptions = array( 
-         ‘http’=>array(
-            ‘method’=>’POST’,
-            ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
-                      .”Authorization: Bearer “.$token.”\r\n”
-                      .”Content-Length: “.strlen($queryData).”\r\n”,
-            ‘content’ => $queryData
+         'http'=>array(
+            'method'=>'POST',
+            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                      ."Authorization: Bearer ".$token."\r\n"
+                      ."Content-Length: ".strlen($queryData)."\r\n",
+            'content' => $queryData
          ),
  );
  $context = stream_context_create($headerOptions);
@@ -23,4 +23,6 @@ function notify_message($message,$token){
  $res = json_decode($result);
  return $res;
 }
-  ?>
+//https://havespirit.blogspot.com/2017/02/line-notify-php.html
+//https://medium.com/@nattaponsirikamonnet/%E0%B8%A1%E0%B8%B2%E0%B8%A5%E0%B8%AD%E0%B8%87-line-notify-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%96%E0%B8%AD%E0%B8%B0-%E0%B8%9E%E0%B8%B7%E0%B9%89%E0%B8%99%E0%B8%90%E0%B8%B2%E0%B8%99-65a7fc83d97f
+?>
